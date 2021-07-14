a2b357b19554c2794fb696b720da7396de443922
e-compfast <alamhafidz01@gmail.com>
2021-07-14 14:47:02 +0700

    add build production files

---
+++ build/precache-manifest.7d816e957b11c3fea57bf86cf5201e8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "579febb97604fe8ff9354cf54186cf64",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a85958a807af3baad71a",
+    "url": "/static/css/main.c74f5793.chunk.css"
+  },
+  {
+    "revision": "45d100f885a84c7c73ee",
+    "url": "/static/js/2.8a912140.chunk.js"
+  },
+  {
+    "revision": "4224b32d50aca7d0b4f248bf0384ed4f",
+    "url": "/static/js/2.8a912140.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a85958a807af3baad71a",
+    "url": "/static/js/main.a9850ac0.chunk.js"
+  },
+  {
+    "revision": "55d488e9e81e555a03c4",
+    "url": "/static/js/runtime-main.077a2d11.js"
+  },
+  {
+    "revision": "4f0b0b8b880c7efb0bab8e787cd2ab66",
+    "url": "/static/media/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.4f0b0b8b.woff2"
+  }
+]);
